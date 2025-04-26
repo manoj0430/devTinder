549e26d7b5fea48e55660f6f0c5e6d6be5cd5cfc
manoj0430 <manojl0430@gmail.com>
2025-04-26 21:22:42 +0530

    Created the server

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the test!!");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello from the root!!");
+});
+app.use("/contact", (req, res) => {
+  res.send("Hello from the contact!!");
+});
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
