642d37dffbb120efa24d85cd2d947ce2bf7fed5b
manoj0430 <manojl0430@gmail.com>
2025-04-28 22:54:15 +0530

    Mutliple Route Checking

--- src/app.js
+++ src/app.js
@@ -2,23 +2,31 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user", (req, res) => {
-  res.send({ firstName: "Lewis", lastName: "Hamilton" });
-});
+//Case 1: Middleware function that sends a response with two request handlers
+// app.use(
+//   "/user",
+//   (req, res, next) => {
+//     console.log("Response 1 is printed");
+//     res.send("Response 1");
 
-app.post("/user", (req, res) => {
-  res.send("Data successfully posted!!");
-});
-app.use("/test", (req, res) => {
-  res.send("Hello from the test!!");
-});
+//   },
+//   (req, res) => {
+//     console.log("Response 2 is printed");
+//     res.send("Response 2");
+//   }
+// );
 
-app.use("/", (req, res) => {
-  res.send("Hello from the root!!");
-});
-app.use("/contact", (req, res) => {
-  res.send("Hello from the contact!!");
-});
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("Response 1 is printed");
+  },
+  (req, res) => {
+    console.log("Response 2 is printed");
+    res.send("Response 2");
+  }
+);
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
